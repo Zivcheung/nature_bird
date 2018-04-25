2b65339997c944da4b320ebac74c74df821042a7
Tianzhu <zivcheung@126.com>
2018-04-25 17:51:15 +1000

    created wrapper and bird class

---
+++ Nature/src/nature/Bird.java
@@ -0,0 +1,29 @@
+package nature;
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class Bird{
+	PApplet parent;
+	PVector position,speed,accelaration;
+	float life_time;
+	boolean alive;
+	Bird(PApplet a){
+		parent=a;
+		
+	}
+	
+	public void add_force(){
+		
+		
+	}
+	
+	putblic void
+	
+	
+	
+	
+	
+	
+	
+}
+}
---
+++ Nature/src/nature/Processing_wrap.java
@@ -0,0 +1,23 @@
+package nature;
+import processing.core.PApplet;
+
+
+public class Processing_wrap extends PApplet{
+
+	public static void main(String[] args) {
+		// set PApplet to the package
+		PApplet.main("nature.Processing_wrap");
+	}
+	
+	public void settings() {
+		size(800,800);
+	}
+	public void setup() {
+	    background(126);
+	}
+	
+	public void draw() {
+		
+	}
+}
+

2b9e132c1647d23d353016ad6d14d47acb19b6ef
Tianzhu <zivcheung@126.com>
2018-04-25 20:45:24 +1000

    add opengl and also start to test 3d object

--- Nature/src/nature/Bird.java
+++ Nature/src/nature/Bird.java
@@ -4,20 +4,63 @@ import processing.core.PVector;
 
 public class Bird{
 	PApplet parent;
-	PVector position,speed,accelaration;
-	float life_time;
-	boolean alive;
+	PVector position,velocity,acceleration;
+	float max_speed;
+	//float life_time;
+	private boolean alive;
 	Bird(PApplet a){
 		parent=a;
+		velocity=new PVector(0,0,0);
+		position=new PVector(0,0,0);
+		acceleration=new PVector(0,0,0);
+		alive=true;
+		max_speed=4;
+		//life_time=50;
+	}
+	
+	public void set_velocity(PVector v) {
+		velocity=v;
+	}
+	
+	private void add_force(PVector a){
+		acceleration=acceleration.add(a);
 		
 	}
 	
-	public void add_force(){
+	
+	public void steer(PVector target) {
+		PVector desire=PVector.sub(target,position);
+		desire.normalize();
+		desire.mult(max_speed);
+		PVector steerforce=PVector.sub(desire,velocity);
 		
+		add_force(steerforce);
 		
 	}
+	public void update() {
+		velocity.add(acceleration);
+		velocity.limit(max_speed);
+		position.add(velocity);
+		clr_force();
 	
-	putblic void
+	}
+	
+	public void show() {
+		parent.pushMatrix();
+		parent.lights();
+		parent.noStroke();
+		parent.translate(position.x,position.y,position.z);
+		parent.sphere(20);
+		parent.popMatrix();
+	}
+
+	public boolean is_live() {
+		return alive;	
+	}
+	
+	public void clr_force() {
+		acceleration.set(0f,0f,0f);
+	}
 	
 	
 	
@@ -25,5 +68,8 @@ public class Bird{
 	
 	
 	
-}
+	
+	
+	
+	
 }
--- Nature/src/nature/Processing_wrap.java
+++ Nature/src/nature/Processing_wrap.java
@@ -1,22 +1,38 @@
 package nature;
 import processing.core.PApplet;
+import processing.core.PVector;
+import nature.Bird;
+//import processing.event.*;
 
 
 public class Processing_wrap extends PApplet{
-
+	Bird bird_a;
+	
 	public static void main(String[] args) {
 		// set PApplet to the package
 		PApplet.main("nature.Processing_wrap");
 	}
 	
 	public void settings() {
-		size(800,800);
+		size(800,800,P3D);
 	}
 	public void setup() {
-	    background(126);
+	   // background(126);
+	   // bird_a=new Bird(this);
+	    //bird_a.set_velocity(new PVector(200,0,0));
 	}
 	
 	public void draw() {
+		pushMatrix();
+		lights();
+		noStroke();
+		translate(50,50,50);
+		sphere(20);
+		popMatrix();
+		//PVector target=new PVector(500,500,500);
+		//bird_a.steer(target);
+		//bird_a.update();
+		//bird_a.show();
 		
 	}
 }
